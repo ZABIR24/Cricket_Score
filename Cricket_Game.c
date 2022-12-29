47184704e5b9988b21353f0428804792b74bff19
ZABIR24 <121471767+ZABIR24@users.noreply.github.com>
2022-12-28 19:38:42 -0800

    Update Cricket_Game.c

--- Cricket_Game.c
+++ Cricket_Game.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int win(int x,int y)
+
+int Win_Status(int AI_Score,int player_score)
 {
     int AI_Score,player_Score,Remaining_Players,TURN;
     if(AI_Score<player_Score || TURN <=6)
@@ -21,8 +22,21 @@ int win(int x,int y)
         return -1;
     }
 }
-int Score_Board()
+
+int AI_Score_Board(int RunI,int WicketI)
 {
+    int Score_Run,Score_Wicket;
+    printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd\n");
+    printf("/xcd/xcd/xcd SCORE BOARD xcd/xcd/xcd/xcd/xcd\n");
+    printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd");
+    printf("\n");
+    printf("RUN:%d\t\t",Score_Run);
+    printf("WICKET:%d\t\n",Score_Wicket);
+}
+
+int Player_Score_Board(int RunP,int WicketP)
+{
+    int Score_Run,Score_Wicket,Remaining_Runs;
     printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd\n");
     printf("/xcd/xcd/xcd SCORE BOARD xcd/xcd/xcd/xcd/xcd\n");
     printf("/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd/xcd");
@@ -31,6 +45,7 @@ int Score_Board()
     printf("WICKET:%d\t\n",Score_Wicket);
     printf("REMAINING RUNS:%d",Remaining_Runs);
 }
+
 int run()
 {
     int points;
@@ -39,6 +54,7 @@ int run()
     points = rand()%6;
     return points;
 }
+
 int Wicket()
 {
     int out;
@@ -50,29 +66,38 @@ int Wicket()
 
 int main()
 {
-    time_t t;
-    int Runs[10]={0,1,2,4,6},AI_SCORE,PLAYER_SCORE;
-    char out[20]={"RunOut","BOLD","Catch"};
-    int R,O,hit;
+    int AI_Wicket[10]={1,5,3,7,4};
+    int AI_SCORE[10]={24,20,15,6,8,12,6,11,9};
+    time_t t,t3;
+    int TURN=0;
+    int Runs[10]={0,1,2,4,6},PLAYER_SCORE=0;
+    char out[20]={"RunOut","BOLD","Catch"},Player_Status;
+    int PR,PO,hit;
+    int RI,OI,win=-1,Remaining_Player=4;
     srand((unsigned)time(&t));
-    printf("Player turn:");
-    hit = rand()%3;
-    switch (hit)
+    RI = rand()%10;
+    OI = rand()%6;
+    AI_Score_Board(AI_SCORE[RI],AI_Wicket[OI]);
+    do
     {
-    case 1:
-        while (AI_SCORE>PLAYER_SCORE)
+        //hit = getch();
+        srand((unsigned)time(&t3));
+        hit = rand()%2;
+        if (hit == 0)
         {
-            printf("");
-            R=rand()%6;
-            int Total_Score =Runs[R];
+            PO = Wicket();
+            Player_Status = out[PO];
+            Remaining_Player-=1;
         }
-        
-        break;
-    case 2:
-
-        break;
-    default:
-        break;
-    }
-    return 0;
+        else
+        {
+            PR =run();
+            int RUNS= Runs[PR];
+            PLAYER_SCORE += RUNS;
+        }
+        TURN++;
+        Player_Score_Board(PLAYER_SCORE,TURN);
+        Win_Status(AI_SCORE,PLAYER_SCORE);
+    } while (win = -1);
+return 0;
 }
